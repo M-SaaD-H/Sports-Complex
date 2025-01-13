d561b9269a89abace437764ebcaef8f51f0890b9
DevSahuIit <devsahu2966@gmail.com>
2025-01-13 21:33:12 +0530

    hogaya

--- src/app.js
+++ src/app.js
@@ -20,10 +20,13 @@ app.use(cookieParser());
 
 import path from "path"
 // Routes
-app.set("view engine","ejs")
 const __filename = new URL('', import.meta.url).pathname;
 const __dirname = path.dirname(__filename);
-app.set(path.join(__dirname,'../client/views'))
+app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine","ejs")
+
+
+// app.set(path.join(__dirname,'views'))
 app.get ("/",(req,res)=>{
     res.render("index")
 })
